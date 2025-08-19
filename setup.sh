1779e72478ddc32bc79b4dfd50aae47c4a9a50dc
Sam <sam@Sams-MacBook-Pro.local>
2025-08-19 14:00:39 +0800

    Exploring dataset (1)

--- setup.sh
+++ setup.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# Installing conda env
-# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -p ~
-# bash ~/Miniconda3-latest-Linux-x86_64.sh
-
-# conda create -n SoccerNetv3
-# conda activate SoccerNetv3
-# conda install opencv pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
-# pip install -r requirements.txt
-
 # Downloading SoccerNet-v3D annotation datasets
-mkdir -p data/raw
-curl -L -o data/raw/SNv3D.csv https://github.com/mguti97/SoccerNet-v3D/releases/download/v1.0.0/SNv3D.csv
-curl -L -o data/raw/SoccerNet-v3D.zip https://github.com/mguti97/SoccerNet-v3D/releases/download/v1.0.0/SoccerNet-v3D.zip
-unzip data/raw/SoccerNet-v3D.zip -d data/raw
-rm data/raw/SoccerNet-v3D.zip 
+# mkdir -p data/raw
+# curl -L -o data/raw/SNv3D.csv https://github.com/mguti97/SoccerNet-v3D/releases/download/v1.0.0/SNv3D.csv
+# curl -L -o data/raw/SoccerNet-v3D.zip https://github.com/mguti97/SoccerNet-v3D/releases/download/v1.0.0/SoccerNet-v3D.zip
+# unzip data/raw/SoccerNet-v3D.zip -d data/raw
+# rm data/raw/SoccerNet-v3D.zip 
 
 # Downloading SoccerNet-v3D benchmark weights (YOLOv11 model) provided by the paper
 # mkdir -p weights/benchmark
@@ -23,3 +14,15 @@ rm data/raw/SoccerNet-v3D.zip
 # mkdir -p data/test
 # curl -L -o data/train/SNv3D-train.txt https://github.com/mguti97/SoccerNet-v3D/releases/download/v1.0.0/SNv3D-train.txt
 # curl -L -o data/test/SNv3D-test.txt https://github.com/mguti97/SoccerNet-v3D/releases/download/v1.0.0/SNv3D-test.txt
+
+# Installing conda env
+# mkdir -p ~/miniconda3
+# curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
+# bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+# rm ~/miniconda3/miniconda.sh
+# source ~/miniconda3/bin/activate
+
+# conda create -n ISC-Football
+# conda activate ISC-Football
+conda install opencv pytorch torchvision torchaudio
+pip3 install -r requirements.txt
