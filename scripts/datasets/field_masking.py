932d9b3d563343ede60fb52667e2d8b337914c2d
Sam <sam@r-5-109-25-172.comp.nus.edu.sg>
2025-10-06 10:33:48 +0800

    Change from BCELoss to DiceLoss

--- scripts/datasets/field_masking.py
+++ scripts/datasets/field_masking.py
@@ -5,8 +5,22 @@ from itertools import pairwise
 from .calibration import CalibrationDataset
 
 class MaskingCalibrationDataset(CalibrationDataset):
-    def __init__(self, img_folder, keys, width=None, height=None, mode='none', transform=None, target_transform=None):
+    n_limit: int
+
+    def __init__(self, img_folder, keys, width=None, height=None, n_limit=None, mode='none', transform=None, target_transform=None):
         super().__init__(img_folder, keys, width, height, mode, transform, target_transform)
+        self.n_limit = n_limit
+
+    @classmethod
+    def from_folder(cls, img_folder, width=None, height=None, n_limit=None, mode='none', transform=None, target_transform=None):
+        ds = super().from_folder(img_folder, width, height, mode, transform, target_transform)
+        ds.n_limit = n_limit
+        return ds
+
+    def __len__(self):
+        if self.n_limit:
+            return self.n_limit
+        return super().__len__()
 
     def __getitem__(self, idx):
         image, label = super().__getitem__(idx)
@@ -14,8 +28,8 @@ class MaskingCalibrationDataset(CalibrationDataset):
         mask_img = np.zeros(image.shape[:2])
         for name, coords in label.items():
             for coord in coords:
-                cv2.circle(mask_img, coord, 2, (255, ), 2)
+                cv2.circle(mask_img, coord, 2, (1., ), 2)
             for coord_bef, coord_aft in pairwise(coords):
-                cv2.line(mask_img, coord_bef, coord_aft, (255, ), 2)
+                cv2.line(mask_img, coord_bef, coord_aft, (1., ), 2)
 
         return image, mask_img
